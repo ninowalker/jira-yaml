55d607a296db789dce068896dcaa8716149b43d9
Nino Walker <nino@livefyre.com>
2014-07-25 10:50:45 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ local = lambda x: os.path.join(base, x)
 
 setup(
     name="jira-yaml",
-    version="0.0.1",
+    version="0.1.0",
     author="Nino Walker",
     author_email="nino.walker@gmail.com",
     description=(""),
